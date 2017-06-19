e0509d350ac48d3ba5b3a7e011ca5570e4a2bc4c
Michał Cichoń <michal.cichon@codete.com>
2017-06-19 16:28:43 +0200

    Update protobuf to 0.9.903

--- Package.swift
+++ Package.swift
@@ -7,6 +7,6 @@ let package = Package(
     dependencies: [
         .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 7),
         .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 7),
-        .Package(url: "https://github.com/apple/swift-protobuf.git", Version(0,9,902))
+        .Package(url: "https://github.com/apple/swift-protobuf.git", Version(0,9,903))
     ]
 )
